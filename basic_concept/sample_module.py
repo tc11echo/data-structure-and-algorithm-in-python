b9000534b6615abf1a21dcba5ab971986b4591fc
Thomas Chan <69757520+tc11echo@users.noreply.github.com>
2022-07-22 19:20:52 +0800

    Update sample_module.py

--- basic_concept/sample_module.py
+++ basic_concept/sample_module.py
@@ -1,4 +1,4 @@
-print("sample module imported")
+print("sample module imported") # test only, module shouldn't contain this kind of statement
 
 def func1():
     print("func1 executed")
@@ -6,5 +6,5 @@ def func1():
 def func2():
     print("func2 executed")
     
-if __name__=="__main__":
+if __name__=="__main__": # this like C++ main function, only directly run sample_module.py will run this
     print("sample module main called")
