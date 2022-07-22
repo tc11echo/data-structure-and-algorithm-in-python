74f1df4bff14c33220da2f73fa50de5727b4d9b0
Thomas Chan <69757520+tc11echo@users.noreply.github.com>
2022-07-22 19:05:28 +0800

    Update sample_module.py

--- basic_concept/sample_module.py
+++ basic_concept/sample_module.py
@@ -1,7 +1,10 @@
-print("Sample module imported)
+print("sample module imported)
 
 def func1():
     print("func1 executed")
     
 def func2():
     print("func2 executed")
+    
+if __name__=="__main__":
+    print("sample module main called)
